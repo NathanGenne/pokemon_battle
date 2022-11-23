6bec0f6c47fb373d80531b5661c612774f8f0189
logeoSE <49406040+logeoSE@users.noreply.github.com>
2022-11-23 15:00:49 +0100

    push antoine

--- vue/combat_vue.php
+++ vue/combat_vue.php
@@ -5,7 +5,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-<link rel="stylesheet" href="assets\styles.css">
+<link rel="stylesheet" href="assets/css/styles.css">
 <title>combat</title>
 </head>
 <body>
@@ -17,6 +17,7 @@
             <?php
             $selected = 0;
             $attack = 1;
+            // Récupération 
             $team1 = $_SESSION['team1'];
             $team2 = $_SESSION['team2'];
             
